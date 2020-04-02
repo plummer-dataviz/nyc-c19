5d6cd55fd2b2ba538ac7b1529c47f1904c80732c
Matthew Plummer <plummat@gmail.com>
2020-04-02 15:54:59 -0400

    Updates

---
+++ precache-manifest.50af1b45946c244acce9653561a4cfa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "832661c935eba522e520b157330c49a3",
+    "url": "/nyc-c19/index.html"
+  },
+  {
+    "revision": "22d8e5b2b9ac6219d29c",
+    "url": "/nyc-c19/static/css/main.ae638dc1.chunk.css"
+  },
+  {
+    "revision": "549c0671cadc58808da3",
+    "url": "/nyc-c19/static/js/2.43aca185.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/nyc-c19/static/js/2.43aca185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22d8e5b2b9ac6219d29c",
+    "url": "/nyc-c19/static/js/main.dc0133b4.chunk.js"
+  },
+  {
+    "revision": "9b7c4b4641d058a65884",
+    "url": "/nyc-c19/static/js/runtime-main.cbc58c19.js"
+  }
+]);
